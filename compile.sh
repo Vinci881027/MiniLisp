5734a7699f5a4a708cea18c569279fcc9ea36107
Vinci Hsieh <vinci@xiewenqideMacBook-Pro.local>
2021-01-18 22:07:31 +0800

    rm unnecessary files

--- compile.sh
+++ compile.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
+
+# compile
 bison -d -o y.tab.c fp.y
 gcc -c -g -I.. y.tab.c
 flex -o lex.yy.c fp.l
 gcc -c -g -I.. lex.yy.c
 gcc -o fp y.tab.o lex.yy.o -ll
+
+# remove unnecessary files
+rm y.tab.c
+rm y.tab.h
+rm y.tab.o
+rm lex.yy.c
+rm lex.yy.o
